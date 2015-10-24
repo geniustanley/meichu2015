575b983ff93fc155749ef60b6c6e6e357a7d7ef6
Frank <franks0059@gmail.com>
2015-10-24 16:02:35 +0800

    render

--- Main/views.py
+++ Main/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from Main.models import ReportList
+from Main.models import ReportDetail
+from django.template import RequestContext
 
 # Create your views here.
+
+
+def MainPageList(request):
+	RList = ReportList.objects.all()
+
+	return render_to_response('index.html',RequestContext(request,locals()))
+
+
+def CreateReportDetail():
+	
+	return render_to_response('',RequestContext(request,locals()))
+
+
